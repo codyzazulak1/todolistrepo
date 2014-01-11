627cef7065b6066c6ae6e0cd4db09189310e3b34
codyzazulak1 <codyzazulak1@gmail.com>
2014-01-11 03:50:18 +0000

    DONE

--- style.js
+++ style.js
@@ -1,34 +1,28 @@
 $(document).ready(function() {
 	$('div.title').click(function() {
-		console.log("test");
     	location.reload();
-
 	});
 
+	$('input.add').click(function() {
+		var todo = prompt("Item title: ");
+		$('#containerID')
+			.append('<div class="nerdStuff">' + '<input class="newThing" type="checkbox">' + todo)
+	});
 
+	$('input.remove').click(function(){
+		$("div.nerdStuff > input:checked").parent().remove();
+	});
 
-$('input.add').click(function() {
-	var todo = prompt("Item title: ");
-$('#containerID')
-	.append('<div class="nerdStuff" ><>') 
-    .append(
-       $(document.createElement('input')).attr({
-           id:    'myCheckbox',
-           name:  'myCheckbox',
-           value: 'myValue',
-           type:  'checkbox',
+	$(document).on("click", "div.nerdStuff > input" , function(e) {
+		e.stopPropagation();
+	});
 
-
-       })
-    ).append(todo).append('<br>').append('</div>');
-});
-
-$('input.remove').click(function(){
-	$("div.nerdStuff, input:checked").remove();
-
-});
-
-
-
-
+	$(document).on("click", "div.nerdStuff" , function(e) {
+		e.preventDefault();
+		if ($(e.currentTarget).children("input").prop('checked') == false) {
+			$(e.currentTarget).children("input").prop('checked', true); 
+		} else {
+			$(e.currentTarget).children("input").prop('checked', false);
+		}
+	});
 });
